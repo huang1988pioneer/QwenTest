ed6ba97508b1bb6e789c2a9ef48528fd5bb95bda
goldshoot0720 <goldshoot0720@gmail.com>
2026-08-29 15:04:42 +0800

    Android 版本號調升至 versionCode 6／versionName 1.4.1

--- android/LyricsPlayer/app/build.gradle.kts
+++ android/LyricsPlayer/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.qwentest.lyricsplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.4.1"
     }
 
     buildTypes {
